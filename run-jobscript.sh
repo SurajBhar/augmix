195db4ecff73044f725ab80f5b135ed2ebe2450a
SurajBhar <suraj.unisiegen@gmail.com>
2022-11-06 03:48:13 +0100

    Modified Bash File for sbatch

--- run-jobscript.sh
+++ run-jobscript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --partition=gpu
-#SBATCH --job-name=augmixtest
-#SBATCH --output=cifartest_new.out
+#SBATCH --job-name=convnext_tiny_pt
+#SBATCH --output=convnext_tiny_pt_true.out
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=suraj.bhardwaj@student.uni-siegen.de
 #SBATCH --nodes=1
@@ -11,6 +11,6 @@
 #SBATCH --mem=10GB
 #SBATCH --gres=gpu:1
 
-module load GpuModules
-module load pytorch-py37-cuda11.2-gcc8/1.9.1
-python cifar.py
+#module load GpuModules
+#module load pytorch-py37-cuda11.2-gcc8/1.9.1
+python cifar.py -m convnext_tiny -pt -op SGD -sc LambdaLR
